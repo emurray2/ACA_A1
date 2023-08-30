b91f48dbedf132366d10576f258b293860785dcb
dleinwan <dani.l.wander@gmail.com>
2023-08-30 16:08:38 -0400

    with in class stuff

--- helper.py
+++ helper.py
@@ -2,9 +2,9 @@
 
 import numpy as np
 
-tarray = np.array([1, 1, 2, 2, 3, 3]) #test array
-tblock_size = 2
-thop = 1 # test hop
+tarray = np.array([1, 1, 2, 2, 3, 3, 4]) #test array
+tblock_size = 4
+thop = 2 # test hop
 tfs = 1 # test sampling frequency
 
 def block_audio(x, block_size, hop_size, fs):
@@ -14,25 +14,37 @@ def block_audio(x, block_size, hop_size, fs):
     # returns a matrix xb (dim: NumOfBlocks by blockSize)
     #         a vector timeInSec (dim: NumOfBlocks) - start time of each block
 
+    #x = append_remaining_zeros(x, block_size)
+    print(x)
 
     numSamples = int(len(x)) # total number of samples
     NumOfBlocks = int(numSamples / hop_size - 1)
 
-    xb = np.empty((NumOfBlocks, int(block_size)))
-    section = np.empty((1, block_size))
-    timeInSec = np.empty(NumOfBlocks)
+    xb = np.zeros((NumOfBlocks, int(block_size)))
+    section = np.zeros((1, block_size))
+    timeInSec = np.zeros(NumOfBlocks)
 
     j = 0 # x sample index
 
     for i in range(0, int(NumOfBlocks)):
         section = x[j:(j+block_size)]
         timeInSec[i] = j
-        #print(section)
-        xb[[(i*hop_size)], :] = section
+        print(section.shape)
+        # xb[[(i*hop_size)], :] = section[None, :]
+        xb[i*hop_size, :] =section
         j += hop_size
         
 
     return xb, timeInSec
+
+def append_remaining_zeros(input_vector, block_size):
+    remainder = len(input_vector) % block_size
+    print(np.shape(input_vector))
+    print(remainder)
+    input_vector = np.pad(input_vector, (0, remainder), 'constant')
+
+    print(input_vector)
+    return input_vector
 
 
 def comp_acf(input_vector, is_normalized):
@@ -41,7 +53,7 @@ def comp_acf(input_vector, is_normalized):
     # boolean is_normalized
     
 
-    r = np.correlate(input_vector, input_vector)
+    r = np.corrcoef(input_vector, input_vector)
 
 
     return r
@@ -70,5 +82,5 @@ if __name__ == "__main__":
     print(xb)
     print(timeInSec)
 
-    r = comp_acf(tarray, False)
-    print(r)
+    #r = comp_acf(tarray, False)
+    #print(r)
